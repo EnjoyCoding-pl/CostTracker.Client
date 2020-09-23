ea5ad2cb427b5283d76ebce4340ed1b3a428698a
Piotr Spałek <>
2020-09-23 16:17:09 +0200

    Rename app, fix android build

--- ios/Pods/Target Support Files/Pods-CosTracker_Client-CosTracker_ClientTests/Pods-CosTracker_Client-CosTracker_ClientTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CosTracker_Client_CosTracker_ClientTests : NSObject
-@end
-@implementation PodsDummy_Pods_CosTracker_Client_CosTracker_ClientTests
-@end
---
+++ ios/Pods/Target Support Files/Pods-CostTracker-tvOS/Pods-CostTracker-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CostTracker_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_CostTracker_tvOS
+@end
